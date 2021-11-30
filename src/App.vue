dbd4d7fb370afb7224e580136b3907d3eb767464
Daniel Pinelli <danielpinelli.fullstack@gmail.com>
2021-11-30 11:57:39 -0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <PokemonPage/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PokemonPage from './pages/PokemonPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   PokemonPage
   }
 }
 </script>
@@ -20,7 +19,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #2a75bb;
   margin-top: 60px;
 }
 </style>
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Who's this Pokemon?</h1>
+  <!--TODO: Picture -->
+  <!--TODO: Options -->
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
