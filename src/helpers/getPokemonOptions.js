bef931333b4324ff6a220c65a1bb8d1d0bd2eb27
Daniel Pinelli <danielpinelli.fullstack@gmail.com>
2021-11-30 20:12:06 -0300

    Getting pokemons form API

--- src/helpers/getPokemonOptions.js
+++ src/helpers/getPokemonOptions.js
@@ -5,18 +5,44 @@ const getPokemons = () => {
     return pokemonsArr.map((_, index) => index + 1)
 }
 
-const getPokemonOptions = () => {
+const getPokemonOptions = async () => {
 
     const mixedPokemons = getPokemons().sort(() => Math.random() - 0.5);
-    getPokemonNames(mixedPokemons.splice(0, 4));
+    const pokemons = await getPokemonNames(mixedPokemons.splice(0, 4));
+
+    console.table(pokemons)
 }
 
 const getPokemonNames = async ([a, b, c, d] = []) => {
 
-    const res = await pokemonApi.get(`/1`)
-    console.log(res)
-    // console.log(a, b, c, d)
+    const promiseArr = [
+        pokemonApi.get(`/${a}`),
+        pokemonApi.get(`/${b}`),
+        pokemonApi.get(`/${c}`),
+        pokemonApi.get(`/${d}`)
+    ]
+
+
+    const [p1, p2, p3, p4] = await Promise.all(promiseArr)
+    return [{
+            name: p1.data.name,
+            id: p1.data.id
+        },
+        {
+            name: p2.data.name,
+            id: p2.data.id
+        },
+        {
+            name: p3.data.name,
+            id: p3.data.id
+        },
+        {
+            name: p4.data.name,
+            id: p4.data.id
+        }
+    ]
+
 }
 
 
-export default getPokemonOptions;
+export default getPokemonOptions
